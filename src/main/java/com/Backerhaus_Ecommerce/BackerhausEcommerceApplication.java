fe214aa24443fbb32c4d58f3c6bc77df37c8446a
MicaBazan <micabazan5@gmail.com>
2024-10-29 08:57:55 -0300

    initialize project

---
+++ src/main/java/com/Backerhaus_Ecommerce/BackerhausEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.Backerhaus_Ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackerhausEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackerhausEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Backerhaus_Ecommerce/BackerhausEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Backerhaus_Ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackerhausEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
