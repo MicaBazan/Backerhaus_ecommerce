d006d1f2fa67b966984a11341972e170cb288f1b
MicaBazan <micabazan5@gmail.com>
2024-11-02 11:31:58 -0300

    Familias

--- src/main/java/com/Backerhaus_Ecommerce/controllers/familiaController.java
+++ src/main/java/com/Backerhaus_Ecommerce/controllers/familiaController.java
@@ -15,18 +15,18 @@ public class familiaController {
     @Autowired
     private familiaService familiaservice;
 
-    @GetMapping
+    @GetMapping(path = "/listFamilias")
     public ArrayList<familiaModel> getFamilia(){
         return this.familiaservice.getFamilia();
     }
 
-    @PostMapping
+    @PostMapping(path = "/saveFamilia")
     public familiaModel saveFamilia(@RequestBody familiaModel familia){
         return this.familiaservice.saveFamilia(familia);
     }
 
 
-    @DeleteMapping(path = "/{id_familia}")
+    @DeleteMapping(path = "/deleteFamilia/{id_familia}")
     public String deleteById(@PathVariable("id_familia") Long id){
         boolean ok = this.familiaservice.deleteFamilia(id);
 
@@ -37,7 +37,7 @@ public class familiaController {
         }
     }
 
-    @PutMapping(path = "/{id_familia}")
+    @PutMapping(path = "/updateFamilia/{id_familia}")
     public familiaModel updateFamiliaById(@RequestBody familiaModel request, @PathVariable("id_familia") Long id){
         return this.familiaservice.updateFamiliaById(request, id);
     }
