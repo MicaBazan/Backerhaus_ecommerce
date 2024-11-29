8c08d8a0a41d2656f58289be29ff39a0507fb908
MicaBazan <micabazan5@gmail.com>
2024-11-28 22:37:41 -0300

    Fix: agregue variable (#1)

--- src/main/java/com/Backerhaus_Ecommerce/models/clienteModel.java
+++ src/main/java/com/Backerhaus_Ecommerce/models/clienteModel.java
@@ -1,4 +1,9 @@
 package com.Backerhaus_Ecommerce.models;
 
 public class clienteModel {
+
+    private String nombre;
+    private String descripcion;
+    private float precio;
+    private String imagen;
 }
