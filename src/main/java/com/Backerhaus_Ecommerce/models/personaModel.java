d58faf1625751a61d5c5ba47dcb334d99ac6d772
MicaBazan <micabazan5@gmail.com>
2024-12-03 13:24:56 -0300

    Fix: modelo persona (#1)

---
+++ src/main/java/com/Backerhaus_Ecommerce/models/personaModel.java
@@ -0,0 +1,77 @@
+package com.Backerhaus_Ecommerce.models;
+
+public class personaModel {
+
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+    private String status;
+    private String nameBusiness;
+    private String statusAccount;
+    private String rol;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getNameBusiness() {
+        return nameBusiness;
+    }
+
+    public void setNameBusiness(String nameBusiness) {
+        this.nameBusiness = nameBusiness;
+    }
+
+    public String getStatusAccount() {
+        return statusAccount;
+    }
+
+    public void setStatusAccount(String statusAccount) {
+        this.statusAccount = statusAccount;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+}
